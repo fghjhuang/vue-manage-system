33dfefaac793464ea78cf9d410033bdfe11cebcd
David <justforjobonly@126.com>
2019-03-08 14:15:57 +0800

    update

--- src/components/page/NormalQMag.vue
+++ src/components/page/NormalQMag.vue
@@ -1,86 +1,215 @@
 <template>
-    <section class="main">
+    <div class="table">
         <div class="crumbs">
             <el-breadcrumb separator="/">
-                <el-breadcrumb-item><i class="el-icon-rank"></i> 常见问题</el-breadcrumb-item>
+                <el-breadcrumb-item><i class="el-icon-lx-cascades"></i> 基础表格</el-breadcrumb-item>
             </el-breadcrumb>
         </div>
-        <el-button type="danger" icon="delete" class="handle-del mr10" @click="delAll">批量删除</el-button>
-
-        <el-button type="primary" class=" mr10" @click="addQna">添加常见问题</el-button>
-
-        <el-table :data="qna" border class="table" ref="multipleTable" >
-            <el-table-column type="selection" width="55" align="center"></el-table-column>
-            <el-table-column prop="head" label="标题" sortable width="150">
-            </el-table-column>
-            <el-table-column prop="url" label="url" width="180">
-            </el-table-column>
-            <el-table-column prop="content" label="内容" :formatter="formatter">
-            </el-table-column>
-            <el-table-column label="操作" width="180" align="center">
-                <template slot-scope="scope">
-                    <el-button type="text" icon="el-icon-edit" >编辑</el-button>
-                    <el-button type="text" icon="el-icon-delete" class="red" >删除</el-button>
-                </template>
-            </el-table-column>
-        </el-table>
-        <div class="pagination">
-            <el-pagination background @current-change="handleCurrentChange" layout="prev, pager, next" :total="qnazise">
-            </el-pagination>
+        <div class="container">
+            <div class="handle-box">
+                <el-button type="danger" icon="delete" class="handle-del mr10" @click="delAll">批量删除</el-button>
+                <el-button type="primary" icon="add" @click="search">添加常见问题</el-button>
+            </div>
+            <el-table :data="qna" border class="table" ref="multipleTable" @selection-change="handleSelectionChange">
+                <el-table-column type="selection" width="55" align="center"></el-table-column>
+                <el-table-column prop="head" label="标题" sortable width="150">
+                </el-table-column>
+                <el-table-column prop="url" label="url" width="180">
+                </el-table-column>
+                <el-table-column prop="content" label="内容" :formatter="formatter">
+                </el-table-column>
+                <el-table-column label="操作" width="180" align="center">
+                    <template slot-scope="scope">
+                        <el-button type="text" icon="el-icon-edit" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                        <el-button type="text" icon="el-icon-delete" class="red" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <div class="pagination">
+                <el-pagination background @current-change="handleCurrentChange" layout="prev, pager, next" :total="1000">
+                </el-pagination>
+            </div>
         </div>
-        <!--<div class="container">-->
-            <!--<p>通过指令 v-dialogDrag 使 Dialog 对话框具有可拖拽的功能。</p>-->
-            <!--<br>-->
-            <!--<el-button type="primary" @click="visible = true;">点我弹框</el-button>-->
-        <!--</div>-->
-        <!--<el-dialog v-dialogDrag title="拖拽弹框" center :visible.sync="visible" width="30%">-->
-            <!--我是一个可以拖拽的对话框！-->
-            <!--<span slot="footer" class="dialog-footer">-->
-                <!--<el-button @click="visible = false">取 消</el-button>-->
-                <!--<el-button type="primary" @click="visible = false">确 定</el-button>-->
-            <!--</span>-->
-        <!--</el-dialog>-->
-    </section>
+
+        <!-- 编辑弹出框 -->
+        <el-dialog title="编辑" :visible.sync="editVisible" width="30%">
+            <el-form ref="form" :model="form" label-width="50px">
+                <el-form-item label="日期">
+                    <el-date-picker type="date" placeholder="选择日期" v-model="form.date" value-format="yyyy-MM-dd" style="width: 100%;"></el-date-picker>
+                </el-form-item>
+                <el-form-item label="姓名">
+                    <el-input v-model="form.name"></el-input>
+                </el-form-item>
+                <el-form-item label="地址">
+                    <el-input v-model="form.address"></el-input>
+                </el-form-item>
+
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="editVisible = false">取 消</el-button>
+                <el-button type="primary" @click="saveEdit">确 定</el-button>
+            </span>
+        </el-dialog>
+
+        <!-- 删除提示框 -->
+        <el-dialog title="提示" :visible.sync="delVisible" width="300px" center>
+            <div class="del-dialog-cnt">删除不可恢复，是否确定删除？</div>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="delVisible = false">取 消</el-button>
+                <el-button type="primary" @click="deleteRow">确 定</el-button>
+            </span>
+        </el-dialog>
+    </div>
 </template>
 
 <script>
-export default {
-    data(){
-        return {
-            visible: false,
-            qna: [{
-                head: "红外控制不了",
-                content: "如果红外线控制失败，请检查是否同时有其他红外设备正在使用",
-                url: "http://www.baidu.com"
-            },
-                {
-                    head: "如何添加设备",
-                    content: "点击右上角，然后选择添加设备",
+    export default {
+        name: 'basetable',
+        data() {
+            return {
+                url: './vuetable.json',
+                qna: [{
+                    head: "红外控制不了",
+                    content: "如果红外线控制失败，请检查是否同时有其他红外设备正在使用",
                     url: "http://www.baidu.com"
                 },
-                {
-                    head: "控制设备没响应",
-                    content: "请确认好已经添加设备成功，网关成功连接网络",
-                    url: "http://www.baidu.com"
+                    {
+                        head: "如何添加设备",
+                        content: "点击右上角，然后选择添加设备",
+                        url: "http://www.baidu.com"
+                    },
+                    {
+                        head: "控制设备没响应",
+                        content: "请确认好已经添加设备成功，网关成功连接网络",
+                        url: "http://www.baidu.com"
+                    }
+                ],
+                cur_page: 1,
+                multipleSelection: [],
+                select_cate: '',
+                select_word: '',
+                del_list: [],
+                is_search: false,
+                editVisible: false,
+                delVisible: false,
+                form: {
+                    name: '',
+                    date: '',
+                    address: ''
+                },
+                idx: -1
+            }
+        },
+        created() {
+            this.getData();
+        },
+        computed: {
+            data() {
+                return this.tableData.filter((d) => {
+                    let is_del = false;
+                    for (let i = 0; i < this.del_list.length; i++) {
+                        if (d.name === this.del_list[i].name) {
+                            is_del = true;
+                            break;
+                        }
+                    }
+                    if (!is_del) {
+                        if (d.address.indexOf(this.select_cate) > -1 &&
+                            (d.name.indexOf(this.select_word) > -1 ||
+                                d.address.indexOf(this.select_word) > -1)
+                        ) {
+                            return d;
+                        }
+                    }
+                })
+            }
+        },
+        methods: {
+            // 分页导航
+            handleCurrentChange(val) {
+                this.cur_page = val;
+            },
+            search() {
+                this.is_search = true;
+            },
+            formatter(row, column) {
+                return row.content;
+            },
+            filterTag(value, row) {
+                return row.tag === value;
+            },
+            handleEdit(index, row) {
+                this.idx = index;
+                const item = this.tableData[index];
+                this.form = {
+                    name: item.name,
+                    date: item.date,
+                    address: item.address
+                };
+                this.editVisible = true;
+            },
+            handleDelete(index, row) {
+                this.idx = index;
+                this.delVisible = true;
+            },
+            delAll() {
+                const length = this.multipleSelection.length;
+                let str = '';
+                this.del_list = this.del_list.concat(this.multipleSelection);
+                for (let i = 0; i < length; i++) {
+                    str += this.multipleSelection[i].name + ' ';
                 }
-            ],
-            qnazise:30
+                this.$message.error('删除了' + str);
+                this.multipleSelection = [];
+            },
+            handleSelectionChange(val) {
+                this.multipleSelection = val;
+            },
+            // 保存编辑
+            saveEdit() {
+                this.$set(this.tableData, this.idx, this.form);
+                this.editVisible = false;
+                this.$message.success(`修改第 ${this.idx+1} 行成功`);
+            },
+            // 确定删除
+            deleteRow(){
+                this.tableData.splice(this.idx, 1);
+                this.$message.success('删除成功');
+                this.delVisible = false;
+            }
         }
-    },
-    created() {
-
-        console.log("init");
-
     }
-}
+
 </script>
 
-<style>
+<style scoped>
+    .handle-box {
+        margin-bottom: 20px;
+    }
+
+    .handle-select {
+        width: 120px;
+    }
+    .handle-del{
+
+    }
+    .handle-input {
+        width: 300px;
+        display: inline-block;
+    }
+    .del-dialog-cnt{
+        font-size: 16px;
+        text-align: center
+    }
+    .table{
+        width: 100%;
+        font-size: 14px;
+    }
     .red{
         color: #ff0000;
     }
     .mr10{
         margin-right: 10px;
-        margin-bottom: 5px;
     }
 </style>
+
